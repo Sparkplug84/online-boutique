82385aea9deb03ba473646c3e5c75b2590bb0e54
Mark McClean <mcclean_mark_02@hotmail.com>
2020-09-20 20:01:49 +0000

    Add product view, url and template created

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    # Adding int: to the product detail ensures the url is a number as navigating
+    # add/ will interpret the string as an id and throw an error
+    path('add/', views.add_product, name='add_product'),
 ]
