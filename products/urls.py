57104090ae24fa5c1a5138f85eb32b556d489296
Mark McClean <mcclean_mark_02@hotmail.com>
2020-09-21 20:07:02 +0000

    Product deletion functionality complete

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # add/ will interpret the string as an id and throw an error
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
