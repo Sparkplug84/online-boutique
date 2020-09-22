1899dd2fab54b2223a92e50607f1ad00d16ed8af
Mark McClean <mcclean_mark_02@hotmail.com>
2020-09-22 19:44:26 +0000

    Fixing image selector input

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -7,6 +8,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
