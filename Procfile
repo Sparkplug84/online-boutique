91e9086208650895d3d87ade723e1561b36bd908
Mark McClean <mcclean_mark_02@hotmail.com>
2020-09-24 20:11:57 +0000

    Heroku setup part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn online_boutique.wsgi:application
--- online_boutique/settings.py
+++ online_boutique/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&+(%h%s23_i0sq$ekt2$_^ls$@ms36$-!fsrm!0_f5fd&!l8t^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sparkplug84-online-boutique', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'online_boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
