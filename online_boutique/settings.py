0688d1c3b53cee53968b0610d48f97ea251a1ee8
Mark McClean <mcclean_mark_02@hotmail.com>
2020-09-27 20:12:10 +0000

    Added cache control

--- online_boutique/settings.py
+++ online_boutique/settings.py
@@ -195,6 +195,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'sparkplug-online-boutique'
     AWS_S3_REGION_NAME = 'us-east-1'
